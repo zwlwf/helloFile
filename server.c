0b15c496ceed29a38e3ce6ca08d14f7e3326ba00
zwlwf <1410183@tongji.edu.cn>
2021-02-03 13:27:11 +0800

    modified:   server.c

--- server.c
+++ server.c
@@ -7,11 +7,13 @@ int main() {
 	while(1) {
 		int client_sock = waitConnect(sock); // 等待client连接
 		char flag = readHello(client_sock);  // 判断client发送的头
-		printf("Read reqest : ->%s <--\n", flag==PUSHCHAR? "push" : "pull");
+		printf("Read request : ->%s <--\n", flag==PUSHCHAR? "push" : "pull");
 		if (flag==PULLCHAR) {
 			sendPackage( client_sock, queue_pop(Q) ); // 发包
-		} else { //if (flag==PUSHCHAR) {
+		} else if (flag==PUSHCHAR) {
 			queue_push( Q, recvPackage(client_sock) ); // 收包
+		} else {
+			printf("Unknown request\n");
 		}
 		close(client_sock);
 	}
